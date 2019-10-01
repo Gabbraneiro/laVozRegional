3ef565ecdb2abc9b41cc2b0f61b763126fbb8a1d
Nacho Zampelunghe <nzampelunghe@gmail.com>
2019-10-01 00:45:22 -0300

    Botón mute y set de url a redes sociales.

--- app/src/main/java/com/example/lavozregional_fm909/MainActivity.java
+++ app/src/main/java/com/example/lavozregional_fm909/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.lavozregional_fm909;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import java.io.IOException;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.media.AudioManager;
@@ -9,16 +12,24 @@ import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnBufferingUpdateListener;
 import android.media.MediaPlayer.OnPreparedListener;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.view.View.OnClickListener;
 import android.util.Log;
 import android.widget.Toast;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener{
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     private MediaPlayer player;
     private String url;
     private ImageButton buttonPlay;
+    private ImageButton buttonMute;
+    private ImageView buttonFacebook;
+    private ImageView buttonTwitter;
+    private ImageView buttonInstagram;
     private boolean playing;
+    private boolean muted;
+    private String urlRedSocial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +38,16 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         url = getString(R.string.app_url);
         buttonPlay = findViewById(R.id.btn_play_stop);
         buttonPlay.setOnClickListener(this);
+        buttonMute = findViewById(R.id.btn_mute);
+        buttonMute.setOnClickListener(this);
+        buttonFacebook = findViewById(R.id.facebook);
+        buttonFacebook.setOnClickListener(this);
+        buttonTwitter = findViewById(R.id.twitter);
+        buttonTwitter.setOnClickListener(this);
+        buttonInstagram = findViewById(R.id.instagram);
+        buttonInstagram.setOnClickListener(this);
         playing = false;
+        muted = false;
         // Inicializo el objeto MediaPlayer
         initializeMediaPlayer();
     }
@@ -37,12 +57,27 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
             if (!playing) {
                 startPlaying();
                 playing = true;
-
             } else {
                 stopPlaying();
                 playing = false;
-
             }
+        } else if (v == buttonMute) {
+            if (!muted) {
+                mutate();
+                muted = true;
+            } else {
+                commute();
+                muted = false;
+            }
+        } else if (v == buttonFacebook) {
+            urlRedSocial = "https://www.facebook.com/RCH90.9/";
+            goToSocialNetwork();
+        } else if (v == buttonTwitter) {
+            urlRedSocial = "https://twitter.com/rch909?s=08";
+            goToSocialNetwork();
+        } else if (v == buttonInstagram) {
+            urlRedSocial = "https://www.instagram.com/marcaregistrada.2019/?igshid=ov0r1tdjjabu";
+            goToSocialNetwork();
         }
     }
 
@@ -76,12 +111,27 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         }
     }
 
-    public void stopPlaying(){
+    public void stopPlaying() {
         if (player.isPlaying()) {
             player.stop();
             player.release();
             initializeMediaPlayer();
         }
         buttonPlay.setBackgroundResource(R.drawable.ic_play_circle_filled_black_24dp);
+    }
+
+    public void mutate() {
+        player.setVolume(0, 0);
+        buttonMute.setBackgroundResource(R.drawable.ic_volume_off_black_24dp);
+    }
+
+    public void commute() {
+        player.setVolume(1, 1);
+        buttonMute.setBackgroundResource(R.drawable.ic_volume_up_black_24dp);
+    }
+
+    public void goToSocialNetwork() {
+        Intent browse = new Intent(Intent.ACTION_VIEW, Uri.parse(urlRedSocial));
+        startActivity(browse);
     }
 }
